5a84525c490ad41468661182806a0ae0f109a898
maucamsan <47923785+maucamsan@users.noreply.github.com>
2022-12-01 11:23:01 -0500

    Set namespaces and some UI controllers

---
+++ Assets/Scripts/YesYMao/Helpers/EnumHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YesYMao.Helpers
+{
+
+    public enum CanvasType
+    {
+        Pregame,
+        MainMenu,
+        GameUI,
+        EndScreen,
+        VictoryScreen, 
+        Credits, 
+        PauseScreen,
+        Options
+    }
+
+}
--- Assets/Scripts/YesYMao/Managers/UIManager.cs
+++ Assets/Scripts/YesYMao/Managers/UIManager.cs
@@ -1,23 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using YesYMao.Helpers;
+using YesYMao.UI;
+using System.Linq;
 
 namespace YesYMao.Managers
 {
-
     public class UIManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private List<CanvasController> canvasControllerList;
+        CanvasController lastActiveCanvas;
+        [SerializeField] GameObject[] tutorial = new GameObject[3];
+        Animator animator;
+
+        // protected override void Awake()
+        // {
+        //     //base.Awake();
+        //     canvasControllerList = GetComponentsInChildren<CanvasController>().ToList();
+        //     canvasControllerList.ForEach(x => x.gameObject.SetActive(false));
+        //     SwitchCanvas(CanvasType.Pregame);
+        // }
+      
+    
+    
+        public void SwitchCanvas(CanvasType type)
         {
+            if (lastActiveCanvas != null)
+                lastActiveCanvas.gameObject.SetActive(false);
             
+            CanvasController desiredCanvas = canvasControllerList.Find(x => x.canvasType == type);
+
+            if (desiredCanvas != null)
+            {
+                desiredCanvas.gameObject.SetActive(true);
+                lastActiveCanvas = desiredCanvas;
+            }
+            else
+                Debug.LogWarning("The main menu canvas was not found!");
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
+    
+   
     }
 
 }
